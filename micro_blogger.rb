78fb3bb5569ed03ca87c9c8906b08bd559fc151d
Martin Safer <mtsafer@gmail.com>
2016-06-14 13:48:20 -0700

    Add terminal interface

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -6,19 +6,39 @@ class MicroBlogger
 	def initialize
 		puts "Initializing MicroBlogger"
 		@client = JumpstartAuth.twitter
+		@command_list = ["h: help", "q: quit", "t: tweet"]
 	end
 
 	def tweet(message)
 		if message.length > 140
 			puts "The tweet is too long. ABORT ABORT!"
+		elsif message.strip.empty?
+			puts "There is no tweet to be tweeted. ABORT ABORT!"
 		else
 			@client.update(message)
 		end
 	end
 
+	def run
+		puts "Welcome to the JSL Twitter Client"
+		command = ""
+		while command != "q"
+			printf "enter command (h for help): "
+			input = gets.chomp
+			parts = input.split
+			command = parts[0].downcase
+			parts.delete(command)
+			message = parts.join(" ")
+			case command
+			when "h" then puts @command_list.join(", ")
+			when "q" then puts "Goodbye!"
+			when "t" then tweet(message)
+			else
+				puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("test"*40)
-blogger.tweet("140test"*20)
-blogger.tweet("<140")
+blogger.run
